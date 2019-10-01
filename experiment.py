dfa565239f47d3e03e09b501aa3b1dc9256d45aa
Mark Velednitsky <marvel2010+github@gmail.com>
2019-10-01 11:29:24 -0700

    experiment for 2018_in_economics

--- experiment.py
+++ experiment.py
@@ -1,8 +1,9 @@
 from wikipedia_category_graph import WikipediaCategoryGraph
 
-wikipedia_cat_graph_class_egt = WikipediaCategoryGraph("Extremal graph theory")
-wikipedia_cat_graph_class_gt = WikipediaCategoryGraph("Graph theory")
-wikipedia_cat_graph_class_mh = WikipediaCategoryGraph("Military history")
+# wikipedia_cat_graph_class_egt = WikipediaCategoryGraph("Extremal graph theory")
+# wikipedia_cat_graph_class_gt = WikipediaCategoryGraph("Graph theory")
+# wikipedia_cat_graph_class_mh = WikipediaCategoryGraph("Military history")
+# wikipedia_cat_graph_class_ecohist = WikipediaCategoryGraph("2018 in economics")
 
 # wikipedia_cat_graph_class_egt.construct_graph(depth=1)
 # wikipedia_cat_graph_class_egt.print_node_information()
@@ -15,3 +16,7 @@ wikipedia_cat_graph_class_mh = WikipediaCategoryGraph("Military history")
 # wikipedia_cat_graph_class_mh.construct_graph(depth=1)
 # wikipedia_cat_graph_class_mh.print_node_information()
 # wikipedia_cat_graph_class_mh.print_edge_information()
+
+# wikipedia_cat_graph_class_ecohist.construct_graph(depth=3)
+# wikipedia_cat_graph_class_ecohist.print_node_information()
+# wikipedia_cat_graph_class_ecohist.print_edge_information()
